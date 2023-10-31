6d7dcb302de2dcdaed97ff5a96a0ae99a72e5090
Sayapatri <52479349+Sayapatri@users.noreply.github.com>
2023-10-31 13:29:40 -0400

    Update analyzer.py

--- analyzer.py
+++ analyzer.py
@@ -1,1 +1,18 @@
 
+import scapy.all as scapy
+
+def capture_packets(interface, filter_expression, packet_count):
+    print(f"Capturing {packet_count} packets on interface {interface}...")
+    packets = scapy.sniff(iface=interface, filter=filter_expression, count=packet_count)
+    return packets
+
+def analyze_packets(packets):
+    # Implement packet analysis here based on your project's requirements.
+
+if __name__ == "__main__":
+    interface = "eth0"  # Replace with the name of your network interface.
+    filter_expression = "icmp or tcp or udp"  # Replace with the desired filter expression.
+    packet_count = 10  # Adjust the number of packets to capture.
+
+    captured_packets = capture_packets(interface, filter_expression, packet_count)
+    analyze_packets(captured_packets)
