51127f79dc1095d47a2a01400ef8d0ad4072a0ff
Sayapatri <52479349+Sayapatri@users.noreply.github.com>
2023-10-31 13:30:18 -0400

    Create visualizer.py

---
+++ visualizer.py
@@ -0,0 +1,11 @@
+import matplotlib.pyplot as plt
+
+def create_packet_count_plot(packet_counts, protocols):
+    # Create a plot to visualize packet counts by protocol.
+    # Implement the specific visualization based on your project's requirements.
+
+if __name__ == "__main__":
+    packet_counts = [100, 200, 50]  # Replace with actual packet counts.
+    protocols = ["TCP", "UDP", "ICMP"]  # Replace with actual protocol names.
+
+    create_packet_count_plot(packet_counts, protocols)
